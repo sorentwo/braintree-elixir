ebf00851698f0ffc65eec9efa16af148fd9c8686
Parker Selbert <parker@sorentwo.com>
2016-02-03 09:24:28 -0600

    Load configuration from the environment first

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,10 @@
 use Mix.Config
 
+config :braintree,
+  merchant_id: System.get_env("BRAINTREE_MERCHANT_ID"),
+  public_key: System.get_env("BRAINTREE_PUBLIC_KEY"),
+  private_key: System.get_env("BRAINTREE_PRIVATE_KEY")
+
 try do
   import_config "#{Mix.env}.secret.exs"
 rescue
