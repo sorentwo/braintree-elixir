e0a5385df4b82e21096c157d06200eba590b3002
Ryan Bigg <git@ryanbigg.com>
2016-04-04 17:07:42 +1000

    Add Braintree.Subscription module

---
+++ test/integration/subscription_test.exs
@@ -0,0 +1,16 @@
+defmodule Braintree.Integration.CustomerTest do
+  use ExUnit.Case, async: true
+
+  @moduletag :integration
+
+  alias Braintree.Customer
+  alias Braintree.Subscription
+
+  test "create/1 with a plan_id" do
+    assert {:ok, customer} = Customer.create(%{payment_method_nonce: "fake-valid-nonce"})
+    card = customer.credit_cards |> List.first
+    assert {:ok, subscription } = Subscription.create(%{payment_method_token: card.token, plan_id: "starter"})
+    IO.inspect subscription
+    # assert {:ok, subscription} = Subscription.create()
+  end
+end
