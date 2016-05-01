b5fd44fd9d2663130fa2ae1b22d5890cf6c1dd8d
David Salazar <djsm777@gmail.com>
2016-05-01 13:50:58 -0430

    Added Braintree.Plan.all

--- lib/plan.ex
+++ lib/plan.ex
@@ -1,20 +1,33 @@
 defmodule Braintree.Plan do
   @moduledoc """
   Get information about braintree plans
+
+  For additional reference see:
+  https://developers.braintreepayments.com/reference/request/plan/all/ruby
   """
 
   alias Braintree.HTTP
+  alias Braintree.ErrorResponse, as: Error
 
   @doc """
-  Get all existing plans or a empty list
+  Get all existing plans or a error response
+
+  ## Example
+
+      {:ok, plans} = Braintree.Plan.all()
+
+      plans # All the existing plans
   """
   def all do
-    plans = HTTP.get("plans")
-    case plans do
-      {:ok, result} ->
-        result
-      {:error, _} ->
-        []
+    get_plans = HTTP.get("plans")
+
+    case get_plans do
+      {:ok, %{"plans" => plans}} ->
+        plans
+      {:error, %{"api_error_response" => error}} ->
+        {:error, Error.construct(error)}
     end
+    
   end
+
 end
