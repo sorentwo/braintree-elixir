8f5cab0647e51b37f5db19b25d0b7fec3a81c076
Parker Selbert <parker@sorentwo.com>
2016-05-25 09:21:51 -0500

    Add type definition for transaction

--- lib/transaction.ex
+++ lib/transaction.ex
@@ -4,6 +4,8 @@ defmodule Braintree.Transaction do
 
   To create a transaction, you must include an amount and either a
   payment_method_nonce or a payment_method_token.
+
+  https://developers.braintreepayments.com/reference/response/transaction/ruby
   """
 
   use Braintree.Construction
@@ -11,33 +13,86 @@ defmodule Braintree.Transaction do
   alias Braintree.HTTP
   alias Braintree.ErrorResponse, as: Error
 
-  defstruct add_ons:                            nil,
+  @type t :: %__MODULE__{
+               add_ons:                            [],
+               additional_processor_response:      String.t,
+               amount:                             number,
+               apple_pay_details:                  String.t,
+               avs_error_response_code:            String.t,
+               avs_postal_code_response_code:      String.t,
+               avs_street_address_response_code:   String.t,
+               billing_details:                    Map.t,
+               channel:                            String.t,
+               coinbase_details:                   String.t,
+               created_at:                         String.t,
+               credit_card_details:                Map.t,
+               currency_iso_code:                  String.t,
+               custom_fields:                      Map.t,
+               customer_details:                   Map.t,
+               cvv_response_code:                  String.t,
+               descriptor:                         Map.t,
+               disbursement_details:               Map.t,
+               discounts:                          [],
+               disputes:                           [],
+               escrow_status:                      String.t,
+               gateway_rejection_reason:           String.t,
+               id:                                 String.t,
+               merchant_account_id:                String.t,
+               order_id:                           String.t,
+               payment_instrument_type:            String.t,
+               paypal_details:                     Map.t,
+               plan_id:                            Sting.t,
+               processor_authorization_code:       String.t,
+               processor_response_code:            String.t,
+               processor_response_text:            String.t,
+               processor_settlement_response_code: String.t,
+               processor_settlement_response_text: String.t,
+               purchase_order_number:              String.t,
+               recurring:                          String.t,
+               refund_ids:                         String.t,
+               refunded_transaction_id:            String.t,
+               risk_data:                          String.t,
+               service_fee_amount:                 number,
+               settlement_batch_id:                String.t,
+               shipping_details:                   Map.t,
+               status:                             String.t,
+               status_history:                     String.t,
+               subscription_details:               Map.t,
+               subscription_id:                    String.t,
+               tax_amount:                         number,
+               tax_exempt:                         boolean,
+               type:                               String.t,
+               updated_at:                         String.t,
+               voice_referral_number:              String.t
+             }
+
+  defstruct add_ons:                            [],
             additional_processor_response:      nil,
-            amount:                             nil,
+            amount:                             0,
             apple_pay_details:                  nil,
             avs_error_response_code:            nil,
             avs_postal_code_response_code:      nil,
             avs_street_address_response_code:   nil,
-            billing_details:                    nil,
+            billing_details:                    %{},
             channel:                            nil,
             coinbase_details:                   nil,
             created_at:                         nil,
-            credit_card_details:                nil,
+            credit_card_details:                %{},
             currency_iso_code:                  nil,
-            custom_fields:                      nil,
-            customer_details:                   nil,
+            custom_fields:                      %{},
+            customer_details:                   %{},
             cvv_response_code:                  nil,
-            descriptor:                         nil,
-            discounts:                          nil,
+            descriptor:                         %{},
             disbursement_details:               nil,
-            disputes:                           nil,
+            discounts:                          [],
+            disputes:                           [],
             escrow_status:                      nil,
             gateway_rejection_reason:           nil,
             id:                                 nil,
             merchant_account_id:                nil,
             order_id:                           nil,
             payment_instrument_type:            nil,
-            paypal_details:                     nil,
+            paypal_details:                     %{},
             plan_id:                            nil,
             processor_authorization_code:       nil,
             processor_response_code:            nil,
@@ -49,15 +104,15 @@ defmodule Braintree.Transaction do
             refund_ids:                         nil,
             refunded_transaction_id:            nil,
             risk_data:                          nil,
-            service_fee_amount:                 nil,
+            service_fee_amount:                 0,
             settlement_batch_id:                nil,
-            shipping_details:                   nil,
+            shipping_details:                   %{},
             status:                             nil,
             status_history:                     nil,
-            subscription_details:               nil,
+            subscription_details:               %{},
             subscription_id:                    nil,
-            tax_amount:                         nil,
-            tax_exempt:                         nil,
+            tax_amount:                         0,
+            tax_exempt:                         false,
             type:                               nil,
             updated_at:                         nil,
             voice_referral_number:              nil
@@ -76,7 +131,7 @@ defmodule Braintree.Transaction do
 
       transaction.status # "settling"
   """
-  @spec sale(Map.t) :: {:ok, any} | {:error, Error.t}
+  @spec sale(Map.t) :: {:ok, t} | {:error, Error.t}
   def sale(params) do
     sale_params = Map.merge(params, %{type: "sale"})
 
@@ -99,7 +154,7 @@ defmodule Braintree.Transaction do
 
       transaction.status # "refunded"
   """
-  @spec refund(String.t, Map.t) :: {:ok, any} | {:error, Error.t}
+  @spec refund(String.t, Map.t) :: {:ok, t} | {:error, Error.t}
   def refund(transaction_id, params) do
     case HTTP.post("transactions/#{transaction_id}/refund", %{transaction: params}) do
       {:ok, %{"transaction" => transaction}} ->
@@ -118,7 +173,7 @@ defmodule Braintree.Transaction do
 
       transaction.status # "voided"
   """
-  @spec void(String.t) :: {:ok, any} | {:error, Error.t}
+  @spec void(String.t) :: {:ok, t} | {:error, Error.t}
   def void(transaction_id) do
     case HTTP.put("transactions/#{transaction_id}/void", %{}) do
       {:ok, %{"transaction" => transaction}} ->
@@ -137,7 +192,7 @@ defmodule Braintree.Transaction do
 
       {:ok, transaction} = Transaction.find("123")
   """
-  @spec find(String.t) :: {:ok, any} | {:error, Error.t}
+  @spec find(String.t) :: {:ok, t} | {:error, Error.t}
   def find(transaction_id) do
     case HTTP.get("transactions/#{transaction_id}") do
       {:ok, %{"transaction" => transaction}} ->
