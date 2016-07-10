c3f439307637271f43169fda7e056c27eebe0b1a
Samuel Seay <sam@manuka.co>
2016-07-11 09:59:39 +1200

    Add integration and unit tests for plans.

---
+++ test/integration/plan_test.exs
@@ -0,0 +1,9 @@
+defmodule Braintree.Integration.PlanTest do
+  use ExUnit.Case, async: true
+
+  alias Braintree.Plan
+
+  test "all/0 can successfully get a response" do
+    {:ok, _} = Plan.all
+  end
+end
