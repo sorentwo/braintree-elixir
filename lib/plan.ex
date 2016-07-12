709e797d4778bcd3239ba6d8d4ef481dc1e5c4a0
Sam Seay <sam@manuka.co>
2016-07-13 05:50:53 +1200

    Add Plan.all support (#14)
    Add a Plan module and support for Plan.all

---
+++ lib/plan.ex
@@ -0,0 +1,67 @@
+defmodule Braintree.Plan do
+  @moduledoc """
+  Plans represent recurring billing plans in a Braintree merchant account.
+  The API for plans is read only.
+
+  For additional reference see:
+  https://developers.braintreepayments.com/reference/request/plan/all/ruby
+  """
+
+  use Braintree.Construction
+
+  alias Braintree.HTTP
+  alias Braintree.ErrorResponse, as: Error
+
+  @type t :: %__MODULE__{
+               id:                         String.t,
+               add_ons:                    [],
+               balance:                    String.t,
+               billing_day_of_month:       String.t,
+               billing_frequency:          String.t,
+               created_at:                 String.t,
+               currency_iso_code:          String.t,
+               description:                String.t,
+               discounts:                  [],
+               name:                       String.t,
+               number_of_billing_cycles:   String.t,
+               price:                      String.t,
+               trial_duration:             String.t,
+               trial_duration_unit:        String.t,
+               trial_period:               String.t,
+               updated_at:                 String.t
+             }
+
+  defstruct id:                         nil,
+            add_ons:                    [],
+            balance:                    nil,
+            billing_day_of_month:       nil,
+            billing_frequency:          nil,
+            created_at:                 nil,
+            currency_iso_code:          nil,
+            description:                nil,
+            discounts:                  [],
+            name:                       nil,
+            number_of_billing_cycles:   nil,
+            price:                      nil,
+            trial_duration:             nil,
+            trial_duration_unit:        nil,
+            trial_period:               nil,
+            updated_at:                 nil
+
+  @doc """
+  Get a list of all the plans defined in the merchant account. If there are
+  no plans an empty list is returned.
+
+  ## Example
+    {:ok, plans} = Braintree.Plan.all
+  """
+  @spec all() :: {:ok, t} | [{:error, Error.t}]
+  def all do
+    case HTTP.request(:get, "plans") do
+      {:ok, response} ->
+        {:ok, construct(Map.get(response, "plans", []))}
+      {:error, %{"api_error_response" => error}} ->
+        {:error, Error.construct(error)}
+    end
+  end
+end
---
+++ test/plan_test.exs
@@ -0,0 +1,56 @@
+defmodule Braintree.PlanTest do
+  use ExUnit.Case, async: true
+
+  alias Braintree.Plan
+
+  test "construct/1 constructs a list of plans with nested maps available" do
+    [first_plan, second_plan] = Plan.construct([%{
+      "add_ons" => [],
+      "balance" => nil,
+      "billing_day_of_month" => nil,
+      "billing_frequency" => 1,
+      "created_at" => "2016-07-07T21:28:00Z",
+      "currency_iso_code" => "USD",
+      "description" => "Plan description",
+      "discounts" => [],
+      "id" => "1234",
+      "name" => "Plan name",
+      "number_of_billing_cycles" => nil,
+      "price" => "14.99",
+      "trial_duration" => nil,
+      "trial_duration_unit" => nil,
+      "trial_period" => false,
+      "updated_at" => "2016-07-07T21:28:00Z"
+    }, %{
+      "add_ons" => [%{
+        "amount" => "5.99",
+        "merchant_id" => "12345"
+      }],
+      "balance" => nil,
+      "billing_day_of_month" => nil,
+      "billing_frequency" => 1,
+      "created_at" => "2016-07-07T21:28:00Z",
+      "currency_iso_code" => "USD",
+      "description" => "Plan description",
+      "discounts" => [],
+      "id" => "5678",
+      "name" => "Plan name",
+      "number_of_billing_cycles" => nil,
+      "price" => "14.99",
+      "trial_duration" => nil,
+      "trial_duration_unit" => nil,
+      "trial_period" => false,
+      "updated_at" => "2016-07-07T21:28:00Z"
+    }])
+
+    assert second_plan.discounts == []
+
+    assert first_plan.add_ons == []
+    assert first_plan.discounts == []
+
+    [addon] = second_plan.add_ons
+
+    assert addon["amount"] == "5.99"
+    assert addon["merchant_id"] == "12345"
+  end
+end
