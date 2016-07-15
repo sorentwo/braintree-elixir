1b41c5e59fcadc405f4d2200a109382144482f41
Sam Seay <sam@manuka.co>
2016-07-16 00:23:05 +1200

    Feature/subscription cancel (#16)
    Add Subscription.find/1 with supporting tests

--- test/integration/plan_test.exs
+++ test/integration/plan_test.exs
@@ -1,6 +1,8 @@
 defmodule Braintree.Integration.PlanTest do
   use ExUnit.Case, async: true
 
+  @moduletag :integration
+
   alias Braintree.Plan
 
   test "all/0 can successfully get a response" do
--- test/integration/subscription_test.exs
+++ test/integration/subscription_test.exs
@@ -6,9 +6,31 @@ defmodule Braintree.Integration.SubscriptionTest do
   alias Braintree.Customer
   alias Braintree.Subscription
 
+  def create_test_subscription do
+    {:ok, customer} = Customer.create(%{payment_method_nonce: "fake-valid-nonce"})
+    [card] = customer.credit_cards
+    Subscription.create(%{payment_method_token: card.token, plan_id: "starter"})
+  end
+
   test "create/1 with a plan_id" do
     assert {:ok, customer} = Customer.create(%{payment_method_nonce: "fake-valid-nonce"})
     [card] = customer.credit_cards
-    assert {:ok, _subscription } = Subscription.create(%{payment_method_token: card.token, plan_id: "starter"})
+    assert {:ok, _subscription} = Subscription.create(%{payment_method_token: card.token, plan_id: "starter"})
+  end
+
+  test "find/1 with a subscription_id" do
+    {:ok, subscription} = create_test_subscription
+    assert {:ok, subscription} = Subscription.find(subscription.id)
+
+    assert subscription.plan_id == "starter"
+    assert %Subscription{} = subscription
+  end
+
+  test "cancel/1 with a subscription_id" do
+    {:ok, subscription} = create_test_subscription
+    assert {:ok, subscription} = Subscription.cancel(subscription.id)
+
+    assert subscription.status == "Canceled"
+    assert %Subscription{} = subscription
   end
 end
