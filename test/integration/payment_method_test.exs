2645f823f7ca7cc505d4fe141c495a898eb13db0
Marin Pranjic <marin@entropia.hr>
2016-09-20 00:08:19 +0200

    Fix tests, pass params as map instead of keyword list.

--- test/integration/payment_method_test.exs
+++ test/integration/payment_method_test.exs
@@ -46,7 +46,7 @@ defmodule Braintree.Integration.PaymentMethodTest do
   end
 
   test "create/1 can create a payment method from a vaulted credit card nonce" do
-    {:ok, customer} = Customer.create(
+    {:ok, customer} = Customer.create(%{
       first_name: "Rick",
       last_name: "Grimes",
       credit_card: %{
@@ -54,7 +54,7 @@ defmodule Braintree.Integration.PaymentMethodTest do
         expiration_date: "01/2016",
         cvv: "100"
       }
-    )
+    })
 
     [card] = customer.credit_cards
     {:ok, payment_method_nonce} = PaymentMethodNonce.create(card.token)
