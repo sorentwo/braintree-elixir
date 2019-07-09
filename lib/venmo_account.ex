6968948fb61823a76a7082edf046fc5ac5bf6424
millz <jason.miller@revzilla.com>
2019-07-09 13:33:33 -0400

    update VenmoAccount

--- lib/venmo_account.ex
+++ lib/venmo_account.ex
@@ -5,52 +5,50 @@ defmodule Braintree.VenmoAccount do
 
   use Braintree.Construction
 
-  alias Braintree.HTTP
   alias Braintree.ErrorResponse, as: Error
+  alias Braintree.HTTP
 
   @type t :: %__MODULE__{
-               created_at:           String.t,
-               customer_id:          String.t,
-               global_id:            String.t,
-               image_url:            String.t,
-               token:                String.t,
-               source_description:   String.t,
-               updated_at:           String.t,
-               username:             String.t,
-               venmo_user_id:        String.t,
-               default:              boolean,
-               subscriptions:        []
-             }
+          created_at: String.t(),
+          customer_id: String.t(),
+          global_id: String.t(),
+          image_url: String.t(),
+          token: String.t(),
+          source_description: String.t(),
+          updated_at: String.t(),
+          username: String.t(),
+          venmo_user_id: String.t(),
+          default: boolean,
+          subscriptions: [any]
+        }
 
   defstruct billing_agreement_id: nil,
-            created_at:           nil,
-            customer_id:          nil,
-            global_id:            nil,
-            image_url:            nil,
-            token:                nil,
-            source_description:   nil,
-            updated_at:           nil,
-            username:             nil,
-            venmo_user_id:        nil,
-            default:              false,
-            subscriptions:        []
+            created_at: nil,
+            customer_id: nil,
+            global_id: nil,
+            image_url: nil,
+            token: nil,
+            source_description: nil,
+            updated_at: nil,
+            username: nil,
+            venmo_user_id: nil,
+            default: false,
+            subscriptions: []
+
   @doc """
   Find a venmo account record using `token` or return an error
   response if the token is invalid.
 
   ## Example
 
-      {:ok, venmo_account} = Braintree.VenmoAccount.find(token)
+      {:ok, venmo_account} = Braintree.Venmo.find(token)
   """
-  @spec find(String.t) :: {:ok, t} | {:error, Error.t}
-  def find(token) do
-    case HTTP.get("payment_methods/venmo_account/#{token}") do
-      {:ok, %{"venmo_account" => venmo_account}} ->
-        {:ok, construct(venmo_account)}
-      {:error, %{"api_error_response" => error}} ->
-        {:error, Error.construct(error)}
-      {:error, :not_found} ->
-        {:error, Error.construct(%{"message" => "venmo token is invalid"})}
+  @spec find(String.t(), Keyword.t()) :: {:ok, t} | {:error, Error.t()}
+  def find(token, opts \\ []) do
+    path = "payment_methods/venmo_account/" <> token
+
+    with {:ok, %{"venmo_account" => map}} <- HTTP.get(path, opts) do
+      {:ok, new(map)}
     end
   end
 
@@ -60,20 +58,17 @@ defmodule Braintree.VenmoAccount do
 
   ## Example
 
-      {:ok, venmo_account} = Braintree.VenmoAccount.update(
+      {:ok, venmo_account} = Braintree.Venmo.update(
         token,
         %{options: %{make_default: true}
       )
   """
-  @spec update(String.t, Map.t) :: {:ok, t} | {:error, Error.t}
-  def update(token, params) do
-    case HTTP.put("payment_methods/venmo_account/#{token}", %{venmo_account: params}) do
-      {:ok, %{"venmo_account" => venmo_account}} ->
-        {:ok, construct(venmo_account)}
-      {:error, %{"api_error_response" => error}} ->
-        {:error, Error.construct(error)}
-      {:error, :not_found} ->
-        {:error, Error.construct(%{"message" => "venmo token is invalid"})}
+  @spec update(String.t(), map, Keyword.t()) :: {:ok, t} | {:error, Error.t()}
+  def update(token, params, opts \\ []) do
+    path = "payment_methods/venmo_account/" <> token
+
+    with {:ok, %{"venmo_account" => map}} <- HTTP.put(path, %{venmo_account: params}, opts) do
+      {:ok, new(map)}
     end
   end
 
@@ -83,17 +78,14 @@ defmodule Braintree.VenmoAccount do
 
   ## Example
 
-      {:ok, venmo_account} = Braintree.VenmoAccount.delete(token)
+      {:ok, venmo_account} = Braintree.Venmo.delete(token)
   """
-  @spec delete(String.t) :: {:ok, t} | {:error, Error.t}
-  def delete(token) do
-    case HTTP.delete("payment_methods/venmo_account/#{token}") do
-      {:ok, %{}} ->
-        {:ok, "Success"}
-      {:error, %{"api_error_response" => error}} ->
-        {:error, Error.construct(error)}
-      {:error, :not_found} ->
-        {:error, Error.construct(%{"message" => "venmo token is invalid"})}
+  @spec delete(String.t(), Keyword.t()) :: {:ok, t} | {:error, Error.t()}
+  def delete(token, opts \\ []) do
+    path = "payment_methods/venmo_account/" <> token
+
+    with {:ok, _payload} <- HTTP.delete(path, opts) do
+      :ok
     end
   end
 end
