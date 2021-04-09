a2169821e2338931ddf935f2ab04fb8b61bd3fca
BobbieBarker <sirkemera@gmail.com>
2021-04-09 05:45:16 -0700

    Fix Braintree.Util.atomize/1 (#110)
    An error was being raised when getting an error from Braintree.Search.perform/3 for "validation_failure".
    This line causes the atomize/1 func to be called in the utils module in an attempt to serialize the error. Unfortunately, there was nothing in this function to prevent String.to_atom/1 from being called on atoms resulting in an argument error:

--- lib/util.ex
+++ lib/util.ex
@@ -47,10 +47,15 @@ defmodule Braintree.Util do
 
       iex> Braintree.Util.atomize(%{"a" => 1, "b" => %{"c" => 2}})
       %{a: 1, b: %{c: 2}}
+
+      iex> Braintree.Util.atomize(%{a: 1, b: %{"c" => 2}})
+      %{a: 1, b: %{c: 2}}
   """
   @spec atomize(map) :: map
   def atomize(map) when is_map(map) do
     Enum.into(map, %{}, fn
+      {key, val} when is_atom(key) and is_map(val) -> {key, atomize(val)}
+      {key, val} when is_atom(key) -> {key, val}
       {key, val} when is_map(val) -> {String.to_atom(key), atomize(val)}
       {key, val} -> {String.to_atom(key), val}
     end)
